d32e9239be099bd68e655036b17cab2f73640ee3
rbtkay <robert.g.khayat@gmail.com>
2021-03-11 10:55:03 +0100

    [UPDATE] training model with dropout and data augmentation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,14 +13,19 @@ if(os.path.exists(test_dir)):
 os.mkdir(test_dir)
 os.mkdir(train_dir)
 
+total_train_count = 0 
+total_test_count = 0 
+
 for d in os.listdir(base_dir):
     dogs = os.listdir(f"{base_dir}/{d}")
     print(len(dogs))
     test_len = int(len(dogs) * 0.3)
-    print("test", test_len)
+
+    total_test_count += test_len
 
     train_len = len(dogs) - test_len
-    print("train", train_len)
+    total_train_count += train_len
+
 
     os.mkdir(f"{test_dir}/{d}")
     os.mkdir(f"{train_dir}/{d}")
@@ -31,3 +36,6 @@ for d in os.listdir(base_dir):
     for dog in dogs[test_len:(train_len + test_len)]:  # [pointdepart: nombrediteration]
         shutil.copy(f'{base_dir}/{d}/{dog}', f'{train_dir}/{d}/{dog}')
 
+
+print(total_test_count)
+print(total_train_count)
--- training.py
+++ training.py
@@ -15,13 +15,7 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 train_dir = 'train'
-validation_dir = 'test'
-
-# train_data = []
-# validation_data = []
-train_data = [os.path.join(train_dir, d) for d in os.listdir(train_dir)]
-validation_data = [os.path.join(validation_dir, d)
-                   for d in os.listdir(validation_dir)]
+valid_dir = 'validation'
 
 #######################################################
 ######       Validation des repartitions         ######
@@ -33,10 +27,138 @@ train_df = pd.DataFrame([{
 } for d in os.listdir(train_dir)])
 
 # nombre de chiens par race dans le set d'evaluation
-test_df = pd.DataFrame.from_dict([{
+valid_df = pd.DataFrame.from_dict([{
     "name": d,
-    "count": len(os.listdir(os.path.join(train_dir, d)))
-} for d in os.listdir(train_dir)])
+    "count": len(os.listdir(os.path.join(valid_dir, d)))
+} for d in os.listdir(valid_dir)])
 
 print(train_df)
-print(test_df)
+# print(valid_df)
+
+fig, ax = plt.subplots(figsize=(50,50))
+x = np.arange(len(train_df['name'].values))
+
+
+width = 0.35
+rects1 = ax.bar(x - width/2, train_df['count'].values, width, label='Train')
+rects2 = ax.bar(x + width/2, valid_df['count'].values, width, label='Validation')
+
+ax.set_ylabel('Number of dogs')
+ax.set_title('Number of dogs per breed')
+ax.set_xticks(x)
+ax.set_xticklabels(train_df['name'].values, rotation='vertical')
+ax.legend()
+
+fig.tight_layout()
+plt.show()
+
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+x = layers.Dropout(0.5)(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(120, activation='softmax')(x)
+
+model = Model(img_input, output)
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(
+        rescale=1./255,rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        classes=valid_df.name.values.tolist())
+
+# Flow validation images in batches of 20 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        valid_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        classes=valid_df.name.values.tolist())
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=int(14458 / 20),
+      epochs=30,
+      validation_data=validation_generator,
+      validation_steps=int(6122 / 20),
+      verbose=2)
+
+model.save("model_1.h5")
+
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc, 'b')
+plt.plot(epochs, val_acc, 'r')
+plt.title('Training and validation accuracy')
+
+plt.show()
+
+# def autolabel(rects):
+#     """Attach a text label above each bar in *rects*, displaying its height."""
+#     for rect in rects:
+#         height = rect.get_height()
+#         ax.annotate('{}'.format(height),
+#                     xy=(rect.get_x() + rect.get_width() / 2, height),
+#                     xytext=(0, 3),  # 3 points vertical offset
+#                     textcoords="offset points",
+#                     ha='center', va='bottom')
+
+# autolabel(rects1)
+# autolabel(rects2)
+
